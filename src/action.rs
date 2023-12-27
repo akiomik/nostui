216786bd17b68200fd10b42dc325f33265c31317
Akiomi Kamakura <akiomik@gmail.com>
2023-12-28 06:11:16 +0900

    Add nostr event to actions

--- src/action.rs
+++ src/action.rs
@@ -1,20 +1,22 @@
 use std::{fmt, string::ToString};
 
+use nostr_sdk::prelude::*;
 use serde::{
-  de::{self, Deserializer, Visitor},
-  Deserialize, Serialize,
+    de::{self, Deserializer, Visitor},
+    Deserialize, Serialize,
 };
 use strum::Display;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Display, Deserialize)]
 pub enum Action {
-  Tick,
-  Render,
-  Resize(u16, u16),
-  Suspend,
-  Resume,
-  Quit,
-  Refresh,
-  Error(String),
-  Help,
+    Tick,
+    Render,
+    Resize(u16, u16),
+    Suspend,
+    Resume,
+    Quit,
+    Refresh,
+    Error(String),
+    Help,
+    ReceiveEvent(nostr_sdk::Event),
 }
