a48ae48822e17eaa17ef48b7e729831d92500882
Akiomi Kamakura <akiomik@gmail.com>
2024-01-22 07:28:10 +0900

    Remove Connection and ConnectionProcess

--- src/nostr.rs
+++ src/nostr.rs
@@ -1,13 +1,9 @@
-mod connection;
-mod connection_process;
 mod event;
 mod new_connection;
 pub mod nip10;
 pub mod nip27;
 mod profile;
 
-pub use connection::Connection;
-pub use connection_process::ConnectionProcess;
 pub use event::SortableEvent;
 pub use new_connection::NewConnection;
 pub use profile::Profile;
